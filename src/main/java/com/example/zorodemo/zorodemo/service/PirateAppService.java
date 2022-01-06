b33de3311799a64c23f391bc67894eabf7f0c49f
masrur-ahmed <sm.masrur.ahmed@gmail.com>
2022-01-06 19:11:05 +0600

    add error utility class

--- src/main/java/com/example/zorodemo/zorodemo/service/PirateAppService.java
+++ src/main/java/com/example/zorodemo/zorodemo/service/PirateAppService.java
@@ -1,4 +1,6 @@
 package com.example.zorodemo.zorodemo.service;
 
 public interface PirateAppService {
+
+
 }
---
+++ src/main/java/com/example/zorodemo/zorodemo/util/PirateCrewError.java
@@ -0,0 +1,13 @@
+package com.example.zorodemo.zorodemo.util;
+
+import com.example.zorodemo.zorodemo.service.PirateAppService;
+
+public class PirateCrewError {
+    private PirateCrewError(){
+
+    }
+
+    public static final String BAD_REQUEST= "BAD_REQUEST";
+    public static final String UNKNOWN = "UNKNOWN";
+    public static final String X = "c";
+}
